f61b4144b766a95ec9d87728afaec71b63573acb
Yusa Widyananda <yusakywn@gmail.com>
2020-11-27 21:33:29 +0800

    coba

---
+++ about us.php
@@ -0,0 +1,263 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+	<title>Exova - Checkout</title>
+	<link rel="icon" href="img/icon/344.jpg">
+
+	<style>
+	.zoom {
+	  padding: 50px;
+	  background-color: green;
+	  transition: transform .2s; /* Animation */
+	  width: 200px;
+	  height: 200px;
+	  margin: 0 auto;
+	}
+
+	.zoom:hover {
+	  transform: scale(1.5); /* (150% zoom)*/
+	}
+	</style>
+
+
+	<!-- Google font -->
+    <link href="https://fonts.googleapis.com/css?family=Hind:400,700" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script
+      type="text/javascript"
+      src="https://app.sandbox.midtrans.com/snap/snap.js"
+      data-client-key="SB-Mid-client-yEKQ-Nlud7C0ReM0"
+    ></script>
+
+	<!-- Bootstrap -->
+	<link type="text/css" rel="stylesheet" href="css/bootstrap.min.css" />
+
+	<!-- Font Awesome Icon -->
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+
+	<!-- Custom stlylesheet -->
+	<link type="text/css" rel="stylesheet" href="css/style.css" />
+
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+		  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+</head>
+
+
+<body>
+<div id="preloder">
+        <div class="loader"></div>
+</div>
+
+	<!-- HEADER -->
+	<header>
+
+		<!-- header -->
+		<div id="header" style="box-shadow: 0px 6px 6px -6px rgba(0, 0, 0, 0.175)">
+			<div class="container">
+				<div>
+					<!-- Logo -->
+					<div class="header-logo" style="padding: 15px 0px;">
+						<a class="logo" href="#">
+							<img src="./img/logo.png" alt="">
+						</a>
+						<!-- Mobile nav toggle
+						<li class="nav-toggle">
+							<button><i>Kategori</i></button>
+						</li>
+						 / Mobile nav toggle -->
+					</div>
+                    <!-- /Logo -->
+                    <!-- Search -->
+
+                    <!-- /Search -->
+                </div>
+			<!-- header -->
+		</div>
+		<!-- container -->
+	</header>
+	<!-- /HEADER -->
+
+    <!-- Breadcrumb Begin -->
+    <div class="breadcrumb-option">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="breadcrumb__links">
+                        <a href="./index.html"><i class="fa fa-home"></i> Home</a>
+                        <a href="./blog.html">About Us</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Breadcrumb End -->
+    <!-- Contact Section Begin -->
+    <section class="contact spad">
+        <div class="container">
+            <H2 align="center"> About Us </h2>
+
+								<div class="container">
+
+										     <!--Grid row-->
+						     <div class="row text-center">
+
+
+						        <!--Grid column-->
+						       <div class="col-md-4 mb-3 ">
+
+						         <h2 class="my-5 h2">Co-Founder 1 </h2>
+
+						         <img class="rounded-circle" width="200px" alt="100x100" src="ngurahfix.jpg"
+						           data-holder-rendered="true">
+											 <p class="my-3"> A.A.Ngurah Krisna Putra Dinatta <br>
+											 	ngurahkrisna@exova.id <br>
+												+62 8311 4870 769</p>
+						       </div>
+						       <!--Grid column-->
+
+
+									 <div class="col-md-4 mb-3">
+
+									 	<h2 class="my-5 h2">Founder</h2>
+
+									 	<img class="rounded-circle" width="200px" "view overlay zoom" src="arthafix.jpg"
+									 		data-holder-rendered="true" "mb-7">
+									 		<p class="my-3">I Km Triyana Artha Putra <br>
+									 			artha@exova.id <br>
+									 			+62 8135 3250 61</p>
+									 </div>
+
+
+									 
+									 <div class="col-md-4 mb-3">
+
+						         <h2 class="my-5 h2">Co-Founder 2</h2>
+
+						         <img class="rounded-circle" width="200px" src="mametfix.jpg"
+						           data-holder-rendered="true">
+											 <p class="my-3">I Ketut Slamet Adi Palguna <br>
+											  adipalgunamv@exova.id <br>
+											+62 851-5671-0356 </p>
+						       </div>
+
+									 <div class="col-md-6 mb-3">
+
+						         <h2 class="my-5 h2">Devisi Program</h2>
+
+						         <img class="rounded-circle" width="200px" src="prastafix.jpg"
+						           data-holder-rendered="true">
+											 <p class="my-3"> I Kadek Prasta Dwiutama<br>
+											 	prasta@exova.id <br>
+												+62 8810 3734 6694 </p>
+						       </div>
+
+									 <div class="col-md-6 mb-3">
+
+						         <h2 class="my-5 h2">Devisi Design</h2>
+
+						         <img class="rounded-circle" width="200px" src="yusa2.jpg"
+						           data-holder-rendered="true">
+											 <p class="my-3"> Kadek Yusa Widyananda <br>
+												 yusakywn@exova.id<br>
+											 	+62 8199 1047 22 </p>
+
+						       </div>
+
+						     </div>
+						     <!--Grid row-->
+
+						   </div>
+						 </section>
+<!-- Contact Section End -->
+
+<!-- Footer Section Begin -->
+<footer class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 col-md-6 col-sm-7">
+                <div class="footer__about">
+                    <div class="footer__logo">
+                        <a href="./index.php"><img src="img/logo.png" alt=""></a>
+                    </div>
+                    <p>Pembayaran bisa dilakukan via fintech atau transfer antar bank</p>
+                    <div class="footer__payment">
+                        <a href="#"><img src="img/payment/ovo.jpg" alt="" style="border-radius: 100%;"></a>
+                        <a href="#"><img src="img/payment/dana.jpg" alt="" style="border-radius: 100%;"></a>
+                        <a href="#"><img src="img/payment/jenius.jpg" alt="" style="border-radius: 100%;"></a>
+                        <a href="#"><img src="img/payment/gopay.jpg" alt="" style="border-radius: 100%;"></a>
+                        <a href="#"><img src="img/payment/payment-1.png" alt="" style="border-radius: 100%;"></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-2 col-md-3 col-sm-5">
+                <div class="footer__widget">
+                    <h6>Telusuri</h6>
+                    <ul>
+                        <li><a href="#">Tentang</a></li>
+                        <li><a href="#">Blog</a></li>
+                        <li><a href="#">Kontak</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-2 col-md-3 col-sm-4">
+                <div class="footer__widget">
+                    <h6>Akun</h6>
+                    <ul>
+                        <li><a href="#">Akun Saya</a></li>
+                        <li><a href="#">Orderan</a></li>
+                        <li><a href="#">Checkout</a></li>
+                        <li><a href="#">Wishlist</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-8 col-sm-8">
+                <div class="footer__newslatter">
+                    <h6>Langganan Gratis</h6>
+                    <form action="#">
+                        <input type="text" placeholder="Email">
+                        <button type="submit" class="site-btn">Langganan</button>
+                    </form>
+                    <div class="footer__social">
+                        <a href="#"><i class="fa fa-facebook"></i></a>
+                        <a href="#"><i class="fa fa-twitter"></i></a>
+                        <a href="#"><i class="fa fa-youtube-play"></i></a>
+                        <a href="#"><i class="fa fa-instagram"></i></a>
+                        <a href="#"><i class="fa fa-pinterest"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-12">
+                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                <div class="footer__copyright__text">
+                    <p>Copyright &copy; <script>document.write(new Date().getFullYear());</script> All rights reserved | <a href="https://colorlib.com" target="_blank">PT. Kayana Entertainment</a></p>
+                </div>
+                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- Footer Section End -->
+
+	<!-- jQuery Plugins -->
+	<script src="js/jquery.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/slick.min.js"></script>
+	<script src="js/nouislider.min.js"></script>
+	<script src="js/jquery.zoom.min.js"></script>
+	<script src="js/main.js"></script>
+
+</body>
+
+</html>
